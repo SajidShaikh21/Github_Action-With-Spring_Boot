2a720053024ed1bde197fc6cfcc903d684ccc04f
Sajid <shaikhsajeed375@gmail.com>
2025-09-20 10:20:59 +0530

    code

---
+++ src/main/java/com/javatechie/cicd/GithubCicdActionApplication.java
@@ -0,0 +1,22 @@
+package com.javatechie.cicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class GithubCicdActionApplication {
+
+	@GetMapping("/welcome")
+	public String welcome()
+	{
+		return "Welcome To JavaTechie";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubCicdActionApplication.class, args);
+	}
+
+}
